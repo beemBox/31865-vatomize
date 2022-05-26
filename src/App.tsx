b3d24ec48f4fe993e7c7654449fb303f15cd0d3f
Mariano Silva <silva.mariano.s@gmail.com>
2022-05-26 10:44:11 -0300

    Adding ItemListContainer and Section components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 // import logo from './logo.svg'; //! lo dejo para acordarme de la configuración del tsconfig.json con el component de svg
 import './App.css';
 import Header from './components/organisms/Header/Header'
+import ItemListContainer from './components/container/ItemListContainer'
 
 function App() {
+  //! para probar grabatar
+  const user = {
+    email: 'test@email.com'
+  } 
   return (
     <div className="App">
-      <Header />
+      <Header user={user}/>
+      <ItemListContainer msg="Mensaje en pantalla." />
     </div>
   );
 }
---
+++ src/components/container/ItemListContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section from '../molecules/Section/Section'
+import './ItemListContainer.css'
+
+const ItemListContainer = ({msg}) => {
+  
+  return (
+    <Section msg={msg} />
+  )
+}
+
+export default ItemListContainer
